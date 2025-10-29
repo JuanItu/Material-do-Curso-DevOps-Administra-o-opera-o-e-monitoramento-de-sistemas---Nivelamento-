d5dad7917fb663b3e9b83280f41cd2fceb0f3806
Juan Ignacio Iturralde Pereira <jiturralde@inf.ufpel.edu.br>
2025-10-28 21:53:29 -0300

    Atualização final do projeto

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,5 +1,6 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
@@ -7,15 +8,15 @@ let tentativas = 1;
 
 while(chute != numeroSecreto ){
     
-    chute = prompt('Escolha um número entre 1 e 10');
-    // se chute for igual ao número secreto
+    chute = parseInt(prompt(`Escolha um número entre 1 e ${numeroMaximo}`));
+    
     if (chute == numeroSecreto) {
-       break;
+        break;
     } else {
-        if(chute < numeroSecreto){
-            alert('O chute '+ chute + ' é menor que o numero secreto');
+        if(chute > numeroSecreto){
+            alert('O chute '+ chute + ' é maior que o numero secreto');
         }else{
-            alert(`O numero secreto é maior que o ${chute}`);
+            alert(`O numero secreto é menor que o ${chute}`); 
         }
     }
     tentativas ++;
